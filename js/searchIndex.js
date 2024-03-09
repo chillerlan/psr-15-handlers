6083c34d49667d6e446623f8769f0d5c51d0aa2b
codemasher <codemasher@users.noreply.github.com>
2024-03-09 23:03:48 +0000

    Deploying to gh-pages from @ chillerlan/psr-15-handlers@d2b2e74e6fe3482f7dd2722680f1eb19a39f6a0b 🚀

---
+++ js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\chillerlan\\LibraryTemplate\\Example",
+            "name": "Example",
+            "summary": "",
+            "url": "classes/chillerlan-LibraryTemplate-Example.html"
+        },                {
+            "fqsen": "\\chillerlan\\LibraryTemplate\\Example\u003A\u003Ahello\u0028\u0029",
+            "name": "hello",
+            "summary": "",
+            "url": "classes/chillerlan-LibraryTemplate-Example.html#method_hello"
+        },                {
+            "fqsen": "\\chillerlan\\LibraryTemplateTest\\ExampleTest",
+            "name": "ExampleTest",
+            "summary": "",
+            "url": "classes/chillerlan-LibraryTemplateTest-ExampleTest.html"
+        },                {
+            "fqsen": "\\chillerlan\\LibraryTemplateTest\\ExampleTest\u003A\u003AtestInstance\u0028\u0029",
+            "name": "testInstance",
+            "summary": "",
+            "url": "classes/chillerlan-LibraryTemplateTest-ExampleTest.html#method_testInstance"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\chillerlan\\LibraryTemplate",
+            "name": "LibraryTemplate",
+            "summary": "",
+            "url": "namespaces/chillerlan-librarytemplate.html"
+        },                {
+            "fqsen": "\\chillerlan",
+            "name": "chillerlan",
+            "summary": "",
+            "url": "namespaces/chillerlan.html"
+        },                {
+            "fqsen": "\\chillerlan\\LibraryTemplateTest",
+            "name": "LibraryTemplateTest",
+            "summary": "",
+            "url": "namespaces/chillerlan-librarytemplatetest.html"
+        }            ]
+);
